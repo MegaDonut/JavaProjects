cf20ad1ae3295b9a499b1930180076546503b112
DmitriiPl <Plighig@mail.ru>
2024-04-08 11:51:31 +0300

    initial

---
+++ lab3/dataAccessLayer1/src/main/java/org/itmo/dataaccesslayer1/DataAccessLayer1Application.java
@@ -0,0 +1,13 @@
+package org.itmo.dataaccesslayer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessLayer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAccessLayer1Application.class, args);
+    }
+
+}
---
+++ lab3/dataAccessLayer1/src/test/java/org/itmo/dataaccesslayer1/DataAccessLayer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itmo.dataaccesslayer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessLayer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
